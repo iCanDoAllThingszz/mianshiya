913405fa37088e1083bf2a15d18996156d4697d3
zhaoyu93 <zhaoyu93@meituan.com>
2024-11-03 13:36:45 +0800

    first commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.zy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
